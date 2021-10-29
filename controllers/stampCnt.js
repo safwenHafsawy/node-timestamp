a66118ab3cd28e7e27c08ca38ff5a748db6735b9
safwenHafsawy <hafsawysafwen@gmail.com>
2021-10-29 16:01:05 +0100

    fixed non valid date

--- controllers/stampCnt.js
+++ controllers/stampCnt.js
@@ -1,13 +1,22 @@
 const stamp = (req, res) => {
   let { date } = req.params;
+  //checking if date is valid
+  if (isNaN(new Date(Number(date))) && isNaN(new Date(date))) {
+    return res.status(200).json({ error: "Invalid Date" });
+  }
+  //handling req with unix timestamp
   if (date.search("-") === -1) {
     const new_date = new Date(Number(date));
-    return res.status(200).send({ d: new_date.toUTCString() });
+    return res
+      .status(200)
+      .json({ unix: Number(date), utc: new_date.toUTCString() });
   }
+  //handling req with regular date
   date = new Date(date);
   const utcDate = new Date(date).toUTCString();
-  const unixDate = new Date(date) / 1000;
-  res.status(200).send({ unix: unixDate, utc: utcDate });
+  const unixDate = (new Date(date) / 1000) * 1000;
+
+  res.status(200).json({ unix: unixDate, utc: utcDate });
 };
 
 const currentStamp = (req, res) => {
--- server.js
+++ server.js
@@ -5,13 +5,8 @@ require("dotenv").config();
 const cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-app.use(express.static("./views"));
 const stampRoute = require("./routes/stampRoute");
 app.use("/api", stampRoute);
-
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
-});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT || 8080, function () {
